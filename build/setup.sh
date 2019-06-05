c6d0981d1fa915c6d61fe7df4f4934e9e51150e1
Douglas Hubert <douglas.hubert@yahoo.com.br>
2019-06-05 10:38:50 -0300

    fix: Setup script

--- build/setup.sh
+++ build/setup.sh
@@ -2,4 +2,3 @@
 aws configure set region us-east-1
 aws configure set aws_access_key_id "${AWS_KEY}"
 aws configure set aws_secret_access_key "${AWS_SECRET}"
-#printf "[default]\naws_secret_access_key = %s\naws_access_key_id = %s\n" "${AWS_SECRET}" "${AWS_KEY}" > /root/.aws/credentials
